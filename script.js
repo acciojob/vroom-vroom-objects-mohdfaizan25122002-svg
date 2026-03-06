890c0e03fe08dbdd8dcd6391b31a86f2539fc93e
mohdfaizan25122002-svg <mohdfaizan25122002@gmail.com>
2026-03-06 22:21:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,21 @@
 // Complete the js code
 function Car(make, model) {}
-
-function SportsCar(make, model, topSpeed) {}
-
-// Do not change the code below
 window.Car = Car;
-window.SportsCar = SportsCar;
-
-let car=new SportsCar("Ferrari","Testarossa",200)
 
 Call getMakeModel()
 console.log(car.getMakeModel())
+function SportsCar(make, model, topSpeed) {}
+window.SportsCar = SportsCar;
+let car=new SportsCar("Ferrari","Testarossa",200)
 
 Call getTopSpeed()
 console.log(car.getTopSpeed())
+
+// Do not change the code below
+
+
+
+
+
+
+
