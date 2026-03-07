012371a64fed9c4e0c38ab32752d7cc2a193c4f9
mohdfaizan25122002-svg <mohdfaizan25122002@gmail.com>
2026-03-07 10:53:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,45 @@
-// Complete the js code
-function Car(make, model) {}
+// Car Constructor
+function Car(make, model) {
+  this.make = make;
+  this.model = model;
+}
+
+// Method on Car prototype
+Car.prototype.getMakeModel = function () {
+  return this.make + " " + this.model;
+};
+
 window.Car = Car;
 
-Call getMakeModel()
-console.log(car.getMakeModel())
-function SportsCar(make, model, topSpeed) {}
+
+// SportsCar Constructor
+function SportsCar(make, model, topSpeed) {
+  // Call Car constructor
+  Car.call(this, make, model);
+
+  this.topSpeed = topSpeed;
+}
+
+// Inherit prototype from Car
+SportsCar.prototype = Object.create(Car.prototype);
+
+// Reset constructor
+SportsCar.prototype.constructor = SportsCar;
+
+// SportsCar method
+SportsCar.prototype.getTopSpeed = function () {
+  return this.topSpeed;
+};
+
 window.SportsCar = SportsCar;
-let car=new SportsCar("Ferrari","Testarossa",200)
 
-Call getTopSpeed()
-console.log(car.getTopSpeed())
 
-// Do not change the code below
+// Create object
+let car = new SportsCar("Ferrari", "Testarossa", 200);
+
+// Call methods
+console.log(car.getMakeModel()); // Ferrari Testarossa
+console.log(car.getTopSpeed());  // 200
 
 
 
